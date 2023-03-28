df3f8dc991bed8600d78380e95c68e6128d9cea9
Saba1119 <100400271+Saba1119@users.noreply.github.com>
2023-03-28 14:46:47 +0530

    Update application-start.sh

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "starting go application"
 /home/ubuntu/crudapp/main > /dev/null 2> /dev/null < /dev/null &
+/home/ubuntu/crudapp/imageupload > /dev/null 2> /dev/null < /dev/null &
