88d4ffab0a00de5da64b94b61d4105fd9b36bda7
Saba1119 <100400271+Saba1119@users.noreply.github.com>
2023-03-29 21:15:52 +0530

    Update application-stop.sh

--- scripts/application-stop.sh
+++ scripts/application-stop.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "stopping go application"
 ps -ef | grep main| grep -v grep | awk '{print $2}' | xargs kill
+ps -ef | grep imageupload| grep -v grep | awk '{print $2}' | xargs kill
